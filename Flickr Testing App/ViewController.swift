58e112158ff0ba28378f83768797f8a0f7c0456d
sarah <sarah@sarahs-imac.local>
2018-05-05 16:25:26 +0300

    done

--- Flickr Testing App/ViewController.swift
+++ Flickr Testing App/ViewController.swift
@@ -10,7 +10,7 @@ import UIKit
 
 class ViewController: UIViewController, UISearchBarDelegate {
 
-    fileprivate func populateArray(photoArray: Array <[String: AnyObject]>) {
+   /* fileprivate func populateArray(photoArray: Array <[String: AnyObject]>) {
         for (i,photoDictionary) in photoArray.enumerated() {
             if let imageURLString = photoDictionary["url_m"] as? String {
                  let rect2 = UIScreen.main.bounds.size
@@ -81,8 +81,61 @@ class ViewController: UIViewController, UISearchBarDelegate {
         }
         
     }
-
+*/
+    @IBOutlet weak var scrollView: UIScrollView!
     
-    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        searchFlickrBy("birds")
+        
+    }
+    //MARK: Utility methods
+    func searchFlickrBy(_ searchString:String) {
+        let manager = AFHTTPSessionManager()
+        let searchParameters:[String:Any] = ["method": "flickr.photos.search",
+                                             "api_key": "29dcb4f64ee627c5f6008811d7ff1a6c",
+                                             "format": "json",
+                                             "nojsoncallback": 1,
+                                             "text": searchString,
+                                             "extras": "url_m",
+                                             "per_page": 5]
+        manager.get("https://api.flickr.com/services/rest/",
+                    parameters: searchParameters,
+                    progress: nil,
+                    success: { (operation: URLSessionDataTask, responseObject:Any?) in
+                        if let responseObject = responseObject as? [String: AnyObject] {
+                            print("Response: " + (responseObject as AnyObject).description)
+                            if let photos = (responseObject as AnyObject)["photos"] as? [String: AnyObject] {
+                                if let photoArray = photos["photo"] as? [[String: AnyObject]] {
+                                    let imageWidth = self.view.frame.width
+                                    self.scrollView.contentSize = CGSize(width: imageWidth, height: imageWidth * CGFloat(photoArray.count))
+                                    for (i,photoDictionary) in photoArray.enumerated() {
+                                        if let imageURLString = photoDictionary["url_m"] as? String {
+                                            let imageView = UIImageView(frame: CGRect(x:0, y:320*CGFloat(i), width:imageWidth, height:imageWidth))
+                                            if let url = URL(string: imageURLString) {
+                                                imageView.setImageWith(url)
+                                                self.scrollView.addSubview(imageView)
+                                            }
+                                            
+                                        }
+                                    }
+                                }
+                            }
+                            
+                        }
+        }) { (operation:URLSessionDataTask?, error:Error) in
+            print("Error: " + error.localizedDescription)
+        }
+        
+    }
+    func searchBarSearchButtonClicked(_ searchBar: UISearchBar) {
+        for subview in self.scrollView.subviews {
+            subview.removeFromSuperview()
+        }
+        searchBar.resignFirstResponder()
+        if let searchText = searchBar.text {
+            searchFlickrBy(searchText)
+        }
+    }
 }
 
